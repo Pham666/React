f23ec97a229cf973686a9ddeb698b5e493533b45
zxkl9 <zxkl9@gmail.com>
2025-04-03 09:58:14 +0900

    3주

---
+++ 0320/component/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NotificationList from "./chapter_06/NotificationList";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<NotificationList />);
+NotificationList

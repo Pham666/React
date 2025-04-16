1f66a0f8cd968167ba582686e8a3216584396db7
Pham666 <zxkl9304123@gmail.com>
2025-04-16 21:56:22 +0900

    Add files via upload

---
+++ 0410/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Area from './chapter_07/Area.jsx';
+/*
+import Counter from './chapter_07/Counter.jsx';
+import Counter2 from './chapter_07/Counter2.jsx';
+import Counter3 from './chapter_07/Counter3.jsx';
+import Counter4 from './chapter_07/Counter4.jsx';
+import Counter5 from './chapter_07/Counter5.jsx';
+import Counter6 from './chapter_07/Counter6.jsx';
+*/
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Area />); 
